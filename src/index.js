d5a9b1daf94a6104a21935b2a3829b2a19f97282
Roman Pavlov <roman.pavlov@sharekey.com>
2018-09-27 22:16:21 +0300

    Solve task.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const opening = [];
+  const closing = [];
+
+  for (let br of bracketsConfig) {
+    opening.push(br[0]);
+    closing.push(br[1]);
+  }
+
+  const opened = [];
+
+  while (str) {
+    const bracket = str[0];
+    str = str.slice(1);
+    const closingInd = closing.indexOf(bracket);
+
+    if (closingInd === -1) {
+      opened.push(bracket);
+    }
+    else {
+      if (opened.length === 0) {
+        if (opening.includes(bracket)) {
+          opened.push(bracket);
+        }
+        else {
+          return false;
+        }
+      }
+      else {
+        const lastOpened = opened.pop();
+        if (opening.indexOf(lastOpened) !== closingInd) {
+          if (opening.includes(bracket)) {
+            opened.push(lastOpened, bracket);
+          }
+          else {
+            return false;
+          }
+        }
+      }
+    }
+  }
+
+  if (opened.length !== 0) {
+    return false;
+  }
+  else {
+    return true;
+  }
 }
